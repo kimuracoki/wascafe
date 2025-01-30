f1fe55a0779fb68792fd857d6b5fdd399e0bdf74
kimura.coki <kimura.coki@gmail.com>
2025-01-30 16:20:25 +0900

    Upd loading logo size

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -36,7 +36,7 @@ const LoadingScreen = () => {
                         <motion.img
                             src={logo}
                             alt="ロゴ"
-                            style={{ width: "10vw" }}
+                            style={{ width: "20vw" }}
                             initial={{ opacity: 0, scale: 0 }}
                             animate={{ opacity: 1, scale: 1 }}
                             exit={{ opacity: 0 }}

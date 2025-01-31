fe465e807ec0548206654908130f2945e046a33d
kimura.coki <kimura.coki@gmail.com>
2025-01-31 12:20:03 +0900

    Add sidebar color

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,58 +1,75 @@
-import { Box, Drawer, Icon, List, ListItem, ListItemButton, ListItemIcon, ListItemText, styled, } from "@mui/material";
+import { Box, Drawer, Icon, List, ListItem, ListItemButton, ListItemIcon, ListItemText, styled, useTheme, } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export const SideBar = (props: Props) => {
     const navigate = useNavigate();
-    return (<Drawer
-        variant="temporary"
-        anchor="right"
-        open={props.drawerOpen}
-        onClose={props.onToggleDrawer}
-    >
-        <Box
-            width={250}
-            role="presentation"
-            onClick={props.onToggleDrawer}
+    const theme = useTheme();
+    return (
+        <Drawer
+            variant="temporary"
+            anchor="right"
+            open={props.drawerOpen}
+            onClose={props.onToggleDrawer}
+            sx={{
+                '& .MuiDrawer-paper': {
+                    backgroundColor: theme.palette.primary.main,
+                    color: '#fff'
+                },
+                '& .MuiListItemText-root': {
+                    color: '#fff'
+                },
+                '& .MuiListItemIcon-root': {
+                    color: '#fff'
+                },
+                '& .MuiTypography-root': {
+                    color: '#fff'
+                },
+            }}
         >
-            <DrawerHeader >
+            <Box
+                width={250}
+                role="presentation"
+                onClick={props.onToggleDrawer}
+            >
+                <DrawerHeader >
 
-            </DrawerHeader>
-            <List>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => { navigate('/home'); }}>
-                        <ListItemIcon>
-                            <Icon>home</Icon>
-                        </ListItemIcon>
-                        <ListItemText secondary="home" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => { navigate('/about'); }}>
-                        <ListItemIcon>
-                            <Icon>face</Icon>
-                        </ListItemIcon>
-                        <ListItemText secondary="about" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => { navigate('/menu'); }}>
-                        <ListItemIcon>
-                            <Icon>restaurant</Icon>
-                        </ListItemIcon>
-                        <ListItemText secondary="menu" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={() => { navigate('/contact'); }}>
-                        <ListItemIcon>
-                            <Icon>mail</Icon>
-                        </ListItemIcon>
-                        <ListItemText secondary="contact" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-        </Box>
-    </Drawer>
+                </DrawerHeader>
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => { navigate('/home'); }}>
+                            <ListItemIcon>
+                                <Icon>home</Icon>
+                            </ListItemIcon>
+                            <ListItemText secondary="home" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => { navigate('/about'); }}>
+                            <ListItemIcon>
+                                <Icon>face</Icon>
+                            </ListItemIcon>
+                            <ListItemText secondary="about" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => { navigate('/menu'); }}>
+                            <ListItemIcon>
+                                <Icon>restaurant</Icon>
+                            </ListItemIcon>
+                            <ListItemText secondary="menu" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={() => { navigate('/contact'); }}>
+                            <ListItemIcon>
+                                <Icon>mail</Icon>
+                            </ListItemIcon>
+                            <ListItemText secondary="contact" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Box>
+        </Drawer>
     )
 };
 
